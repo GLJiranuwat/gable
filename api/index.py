73c2146a1983cfd500d76c7be7cdbf9be132f692
Jiranuwat <jiranuwat.tima@graphlogic.ai>
2025-04-23 18:12:08 +0700

    add deployment

--- api/index.py
+++ api/index.py
@@ -1,20 +1,20 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 from readability import Document
-from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 GOOGLE_API_KEY = 'AIzaSyDcTr-I7uai-_FiDaUlooYOw0ImTClo9mA'
-SEARCH_ENGINE_ID = '83dd639853d3e4a2b&q'
+SEARCH_ENGINE_ID = '83dd639853d3e4a2b'
 
 def google_search(query):
-    url = f'https://www.googleapis.com/customsearch/v1?key=AIzaSyDcTr-I7uai-_FiDaUlooYOw0ImTClo9mA&cx=83dd639853d3e4a2b&q={query}'
+    url = f'https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}'
     res = requests.get(url).json()
-    print(res['items'][0]['link'])
-    return res['items'][0]['link'] if 'items' in res else None
+    print(res.get('items', [{}])[0].get('link'))
+    return res.get('items', [{}])[0].get('link')
 
 def extract_clean_content(url):
     res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -39,5 +39,5 @@ def extract():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run()
+# Vercel will look for this
+app = app
